59c9e6b19ca4d7c57153ec3ec2da3653e5dc7c66
Tomas Mihalicka <tomas@mihalicka.com>
2026-03-06 16:08:06 +0100

    feat(demo): add Kernel, bundles.php, and all config packages

---
+++ examples/symfony-demo/config/bundles.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class  => ['all' => true],
+    Monadial\Nexus\Symfony\NexusBundle::class             => ['all' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+];
