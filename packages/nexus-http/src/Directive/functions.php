fd0b32d9e216377f009a40c409864c8b2c68ec20
Tomas Mihalicka <tomas@mihalicka.com>
2026-04-28 19:18:02 +0200

    feat(http): bootstrap directive functions file

--- packages/nexus-http/src/Directive/functions.php
+++ packages/nexus-http/src/Directive/functions.php
@@ -2,4 +2,7 @@
 
 declare(strict_types=1);
 
-// Directive DSL helper functions are defined in subsequent tasks.
+namespace Monadial\Nexus\Http;
+
+// Directive functions are appended to this file by the tasks that follow.
+// All directives live in this single namespace and are autoloaded as composer "files".
