4222208d4ac380ef7466891f8e3872c25aa9e21f
Tomas Mihalicka <tomas@mihalicka.com>
2026-06-13 01:32:12 +0200

    feat(http-ws): ChannelActorRegistry

---
+++ packages/nexus-http-ws/src/WebSocket/ChannelActorRegistry.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Monadial\Nexus\Http\Ws\WebSocket;
+
+use Monadial\Nexus\Core\Actor\ActorRef;
+use Monadial\Nexus\Core\Actor\ActorSystem;
+use Monadial\Nexus\Core\Actor\Props;
+
+/**
+ * @psalm-api
+ *
+ * Spawns and caches one channel actor per stable name. Used by
+ * WebSocketDispatcher; not part of the user-facing API.
+ */
+final class ChannelActorRegistry
+{
+    /** @var array<string, ActorRef> */
+    private array $refs = [];
+
+    public function __construct(private readonly ActorSystem $system) {}
+
+    public function resolveOrSpawn(string $name, Props $props): ActorRef
+    {
+        if (isset($this->refs[$name])) {
+            return $this->refs[$name];
+        }
+
+        $ref = $this->system->spawn($props, $name);
+        $this->refs[$name] = $ref;
+
+        return $ref;
+    }
+
+    public function remove(string $name): void
+    {
+        if (isset($this->refs[$name])) {
+            $ref = $this->refs[$name];
+            unset($this->refs[$name]);
+            $this->system->stop($ref);
+        }
+    }
+}
