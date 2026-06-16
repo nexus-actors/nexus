da7ed403e49a8037f7e18307978540a3294f6657
Tomas Mihalicka <tomas@mihalicka.com>
2026-06-16 20:24:23 +0200

    feat(doctrine-dbal): add ConnectionFactory + DriverManager impl

---
+++ packages/nexus-doctrine-dbal/tests/Support/StubConnectionFactory.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Monadial\Nexus\Doctrine\Dbal\Tests\Support;
+
+use Doctrine\DBAL\Connection;
+use Monadial\Nexus\Doctrine\Dbal\Pool\ConnectionFactory;
+use Override;
+use PHPUnit\Framework\MockObject\Generator\Generator;
+use RuntimeException;
+
+/**
+ * Returns mock Connection instances. Tracks creations and allows tests
+ * to inject pre-prepared mocks. Used by ConnectionPool unit tests (T9+).
+ *
+ * @psalm-api
+ */
+final class StubConnectionFactory implements ConnectionFactory
+{
+    public int $creations = 0;
+
+    /** @var list<Connection> */
+    private array $prepared = [];
+
+    public function prepend(Connection $conn): void
+    {
+        $this->prepared[] = $conn;
+    }
+
+    #[Override]
+    public function create(): Connection
+    {
+        $this->creations++;
+
+        if ($this->prepared !== []) {
+            return array_shift($this->prepared);
+        }
+
+        /** @var Connection $mock */
+        $mock = (new Generator())->testDouble(
+            Connection::class,
+            true,
+            null,
+            [],
+            '',
+            false,
+        );
+
+        return $mock;
+    }
+
+    public function exhaustOrFail(): void
+    {
+        if ($this->prepared !== []) {
+            throw new RuntimeException('Prepared connections not all consumed');
+        }
+    }
+}
