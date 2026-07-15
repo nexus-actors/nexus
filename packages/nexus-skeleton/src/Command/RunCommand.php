cc747175a7725c3f73a39419944c547e4decf548
Tomas Mihalicka <tomas@mihalicka.com>
2026-07-15 15:19:39 +0200

    feat(skeleton): bin/console + nexus:run; update composer dependencies
    - Add bin/console entry point with Symfony Console Application
    - Add App\Command\RunCommand implementing nexus:run command
    - Update composer.json with path repositories for monorepo packages
    - Fix composer.json version constraints to match dev-main branches
    - Ensure Symfony dependencies ^7.0 to match nexus-app requirements

---
+++ packages/nexus-skeleton/src/Command/RunCommand.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+use App\Kernel;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand('nexus:run', 'Boot the actor system and run until shutdown')]
+final class RunCommand extends Command
+{
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln('<info>Booting Nexus actor system…</info>');
+        $appName = (string) (getenv('APP_NAME') !== false ? getenv('APP_NAME') : 'my-app');
+        new Kernel(\dirname(__DIR__, 2), $appName)->run();
+
+        return Command::SUCCESS;
+    }
+}
