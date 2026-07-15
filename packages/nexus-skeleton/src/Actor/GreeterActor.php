b2f22d583457b24e20870cb732d43d78e7705564
Tomas Mihalicka <tomas@mihalicka.com>
2026-07-15 14:46:37 +0200

    feat(skeleton): minimal composer + config + sample #[AsActor] greeter

---
+++ packages/nexus-skeleton/src/Actor/GreeterActor.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actor;
+
+use App\Message\Greet;
+use App\Support\Recorder;
+use Monadial\Nexus\App\AsActor;
+use Monadial\Nexus\Core\Actor\ActorContext;
+use Monadial\Nexus\Core\Actor\ActorHandler;
+use Monadial\Nexus\Core\Actor\Behavior;
+use Override;
+
+/**
+ * @implements ActorHandler<object>
+ */
+#[AsActor('greeter')]
+final readonly class GreeterActor implements ActorHandler
+{
+    public function __construct(private Recorder $recorder) {}
+
+    #[Override]
+    public function handle(ActorContext $ctx, object $message): Behavior
+    {
+        if ($message instanceof Greet) {
+            $this->recorder->greeted[] = $message->name;
+        }
+
+        return Behavior::same();
+    }
+}
